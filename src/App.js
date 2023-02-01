1b0bb21d944072a8ac7ec724fc3b845e9391e48a
Yuanmeng Liu <yuanmeng.liu@r2-factory.com>
2023-02-01 14:50:36 +0000

    GraphQL for Front End Developers use apollo client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+import {useQuery, gql} from '@apollo/client';
+import React , {useState} from 'react';
+
+export const AnimeList = gql`
+query Query($page: Int) {
+  Page(page: $page) {
+    media {
+      siteUrl
+      title {
+        english
+        native
+      }
+      description
+      coverImage {
+        medium
+      }
+      bannerImage
+      volumes
+      episodes
+    }
+  }
+}
+`;
+
 function App() {
+  const [page, setPage] = useState(1);
+  const {loading, error, data} = useQuery(AnimeList , {  variables: { "page" : page } });
+  const NextPage = () => {
+    setPage(page+1);
+  }
+  const PreviousPage = () => {
+    setPage(page - 1);
+  }
+  console.log(data?.Page?.media[0]); 
+  if(loading) return(<> Loading</>);
+  if(error) return(<>{JSON.stringify(error)}</>)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <div className="container"> 
+     <h1> 🐈 Anime List </h1>
+     <hr width="100%" />
+   {data?.Page?.media.map(anime => (
+     <>
+   <div className="card" >
+      <img    src={anime.coverImage.medium} alt=""/>
+      <div> 
+         <h1>{anime.title.english} </h1>
+           <div className="episodes" >Episodes  <b>{anime.episodes} </b></div>
+          <div  dangerouslySetInnerHTML={{__html: anime.description}} ></div> 
+      </div> 
+  </div>
+  <hr width="100%"/>
+ </>
+   ))}
+   <div className="buttonContainer">
+    { page !== 1 && <button onClick={PreviousPage}> Previous Page</button> } 
+     <div className="pageText"> {page}</div>
+     <button onClick={NextPage}>  Next Page </button> 
+   </div>
+   </div>);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const client = new ApolloClient({
+  uri : 'https://graphql.anilist.co/',
+  cache: new InMemoryCache(),
+})
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById('root')
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
